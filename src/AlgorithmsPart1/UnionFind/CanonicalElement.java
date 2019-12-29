f4f35efd482c237873dde9e04884dde7b9447a3d
Atul Joshi <ajoshi.biz@gmail.com>
2019-12-29 22:40:27 +0530

    canoncial item

--- src/AlgorithmsPart1/UnionFind/CanonicalElement.java
+++ src/AlgorithmsPart1/UnionFind/CanonicalElement.java
@@ -2,12 +2,52 @@ package AlgorithmsPart1.UnionFind;
 
 public class CanonicalElement {
 
-    private int[] id;
+    private static int[] parent;
+    private static int[] rank;
 
     private CanonicalElement(int N) {
-        id = new int[N];
+        parent = new int[N];
         for (int i = 0; i < N; i++) {
-            id[i] = i;
+            parent[i] = i;
+            rank[i] = 1;
         }
     }
+
+    private static boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private static int root(int a) {
+        while (a != parent[a]) {
+            a = parent[a];
+        }
+        return a;
+    }
+
+    private static void union(int a, int b) {
+
+        int rootA = root(a);
+        int rootB = root(b);
+
+        if (rootA == rootB) {
+            return;
+        }
+
+        if(rank[rootA] < rank[b]){
+            parent[rootA] = rootB;
+            rank[rootB] = rank[rootB] + rank[rootA];
+        } else {
+            parent[rootB] = rootA;
+            rank[rootA] =  rank[rootA] + rank[rootB];
+        }
+
+
+    }
+
+    private static void find(int a) {
+
+    }
+
+
 }
+
